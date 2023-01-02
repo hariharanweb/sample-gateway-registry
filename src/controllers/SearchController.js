5e268385dd4e42c02c6b2d7c22cf7352df6989a7
shashank.sharma <Shashank_A_Sharma@gap.com>
2023-01-02 18:39:57 +0530

    modify control flow of Authorize using then chaining
    as per ES8 updated the Authorization flow using then and catch statement rather than rudimentary if else

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -1,34 +1,30 @@
-import Api from "../api/Api";
-import registry from "../registry/registry.json";
-import LoggingService from "../services/LoggingService";
-import Utils from "../utils/Utils";
-import auth from "../utils/auth";
-// import crypto from "crypto";
-// import fs from "fs";
-// import path from "path";
+import Api from '../api/Api';
+import registry from '../registry/registry.json';
+import LoggingService from '../services/LoggingService';
+import Utils from '../utils/Utils';
+import auth from '../utils/auth';
 
 const search = (req, res) => {
-  const logger = LoggingService.getLogger("Search");
+  const logger = LoggingService.getLogger('Search');
   logger.debug(`Search called with ${JSON.stringify(req.body)}`);
 
-  const authorization = auth.authorize(req);
-
-  if (authorization) {
+  logger.debug('Before Authorize call');
+  auth.authorize(req).then((x) => {
+    logger.debug('On Fullfilled Promise of Authorize call');
     const bppSubscribers = registry.filter(
-      (entry) => entry.status === "SUBSCRIBED" && entry.type === "BPP"
+      (entry) => entry.status === 'SUBSCRIBED' && entry.type === 'BPP',
     );
     bppSubscribers.forEach((bppSubscriber) => {
       logger.debug(`Calling BPP ${bppSubscriber.subscriber_id}`);
       const url = `${bppSubscriber.subscriber_url}/search`;
-  
+
       Api.doPost(url, req.body);
     });
     res.send(Utils.successfulAck);
-  }
-  else {
-    res.status(401).send("Error");
-  }
-
+  }).catch((err) => {
+    logger.debug('On Rejected Promise of Authorize call');
+    res.status(401).send('Error');
+  });
 };
 
 export default {
