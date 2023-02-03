11e1e2b31115cbb9804c96af51136c3d8750dc82
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-02-03 15:31:16 +0530

    [Japneet | Shashank] Modify. subscribeController

--- src/controllers/SubscribeController.js
+++ src/controllers/SubscribeController.js
@@ -2,8 +2,8 @@ import GenericResponse from '../utilities/GenericResponse';
 import SubscribeService from '../services/SubscribeService';
 
 const subscribe = async (req, res) => {
-  GenericResponse.sendAcknowledgement(res);
   SubscribeService.subscribe(req.body.message);
+  GenericResponse.sendAcknowledgement(res);
 };
 
 export default {
