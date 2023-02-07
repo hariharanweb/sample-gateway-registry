3c6ec998ac03d5e529d383780354f8158765b367
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-02-07 16:31:26 +0530

    [Japneet | Priya] Add. mode switching

--- src/services/RegistryService.js
+++ src/services/RegistryService.js
@@ -1,12 +1,32 @@
 import _ from 'lodash';
-import registry from '../registry/registry.json';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const getRegisteredSellerApps = (context) => {
-  const matchingSellerApps = _.filter(registry, { domain: context.domain });
+  // const registry = require('../registry/registry.json')
+  const filename = fileURLToPath(import.meta.url);
+
+  const dirname = path.dirname(filename);
+  const registryFilePath = path.join(dirname, '../registry/registry.json');
+  fs.readFileSync(registryFilePath);
+  const matchingSellerApps = _.filter(registryFilePath, { domain: context.domain });
   return matchingSellerApps;
 };
 
-const getRegistry = (filter) => _.filter(registry, filter);
+const getRegistry = (filter) => {
+  // const registry = require('../registry/registry.json')
+  const filename = fileURLToPath(import.meta.url);
+
+  const dirname = path.dirname(filename);
+  const registryFilePath = path.join(dirname, '../registry/registry.json');
+
+  const value = fs.readFileSync(registryFilePath, { encoding: 'utf8', flag: 'r' });
+  // console.log('hereee');
+  // console.log(value);
+  const result = _.filter(value, filter);
+  return result.replace('//', '');
+};
 export default {
   getRegisteredSellerApps,
   getRegistry,
