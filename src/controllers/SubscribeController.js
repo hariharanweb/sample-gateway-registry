6adc0b4998c2cf942855bd841607f279853c30fd
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-02-09 16:31:52 +0530

    [Japneet | Priya] Refactor. removed console

--- src/controllers/SubscribeController.js
+++ src/controllers/SubscribeController.js
@@ -2,7 +2,6 @@ import GenericResponse from '../utilities/GenericResponse';
 import SubscribeService from '../services/SubscribeService';
 
 const subscribe = async (req, res) => {
-  console.log('IN CONTROLLER \n');
   SubscribeService.subscribe(req.body.message);
   GenericResponse.sendAcknowledgement(res);
 };
--- src/services/RegistryService.js
+++ src/services/RegistryService.js
@@ -2,10 +2,8 @@ import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import LoggingService from './LoggingService';
 
 const getRegistry = (filter) => {
-  const logger = LoggingService.getLogger('RegistryService');
   const filename = fileURLToPath(import.meta.url);
 
   const dirname = path.dirname(filename);
@@ -13,7 +11,6 @@ const getRegistry = (filter) => {
 
   const value = fs.readFileSync(registryFilePath, { encoding: 'utf8' });
   const registry = JSON.parse(value);
-  logger.debug(`In get registry: \n${JSON.stringify(registry)}`);
   return _.filter(registry, filter);
 };
 
